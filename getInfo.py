aa8dc82288edacc4846b5c478035a5b511fd4bfa
jackieji00 <jackieji1014@gmail.com>
2022-08-11 23:53:19 +0800

    新加一个站点

--- getInfo.py
+++ getInfo.py
@@ -21,6 +21,8 @@ class getInfo:
         self.link =link
         req = requests.get(url=link)
         req.encoding = "utf-8"
+        if link.__contains__("yinghuatxt.com/"):
+            req.encoding="gb2312"
         wholePage = req.text
         self.bs = BeautifulSoup(wholePage,'lxml')
         if link.__contains__("m.yushubo.com"):
@@ -35,9 +37,16 @@ class getInfo:
             self.description = self.bs.find(attrs={'property':'og:description'})['content']
             self.auther = self.bs.find(attrs={'property':'og:novel:author'})['content']
             self.cover_link = self.bs.find(attrs={'id':'bookImg'}).contents[0]['src']
+        elif link.__contains__("yinghuatxt.com/"):
+            self.DOMAIN = "https://www.yinghuatxt.com/"
+            self.title = self.bs.find(attrs={'class':'book-info'}).contents[1].contents[1].text
+            # self.description = self.bs.find(attrs={'property':'description'})['content']
+            # self.auther = self.bs.find(attrs={'property':'og:novel:author'})['content']
+            #self.cover_link = self.bs.find(attrs={'id':'bookImg'}).contents[0]['src']
         else:
             print("link is not in the database")
             return
+        print("书名："+self.title)
         self.threadwork=0
         self.lock_lst = _thread.allocate_lock()
         req.close()
@@ -52,7 +61,10 @@ class getInfo:
                 sess.keep_alive = False # 关闭多余连
                 req = requests.get(url=target, stream=True, timeout=(5,5))
                 req.encoding = "utf-8"
+                if self.DOMAIN.__contains__("yinghuatxt.com/"):
+                    req.encoding="gb2312"
                 wholePage = req.text
+                texts=[]
                 bs = BeautifulSoup(wholePage,'lxml')
                 if self.DOMAIN == "https://m.yushubo.com/":
                     content = bs.find('content',id='readContent')
@@ -60,14 +72,6 @@ class getInfo:
                     # delete last tag
                     l = len(texts)
                     del texts[l-3:l]
-                    # save to global value
-                    self.lock_lst.acquire()
-                    d= lst[count]
-                    if not chapter in d:
-                        d[chapter]=[]
-                    if texts not in d[chapter]:
-                        d[chapter].append(texts)
-                    self.lock_lst.release()
                 elif self.DOMAIN == "https://aishuge.la/":
                     content = bs.find('div',id='txt').contents
                     len_content = len(content)-3
@@ -75,14 +79,20 @@ class getInfo:
                     for i in range(len_content):
                         dd = bs.find(attrs={'data-id':str(i)}).contents
                         texts+= dd
-                    # save to global value
-                    self.lock_lst.acquire()
-                    d= lst[count]
-                    if not chapter in d:
-                        d[chapter]=[]
-                    if texts not in d[chapter]:
-                        d[chapter].append(texts)
-                    self.lock_lst.release()
+                elif self.DOMAIN == "https://www.yinghuatxt.com/":
+                    content= bs.find(attrs={'class':'read-content j_readContent'})
+                    if not content is None:
+                        texts=content.contents
+                    else:
+                        print("chapter cannot find "+chapter)
+                # save to global value
+                self.lock_lst.acquire()
+                d= lst[count]
+                if not chapter in d:
+                    d[chapter]=[]
+                if texts not in d[chapter]:
+                    d[chapter].append(texts)
+                self.lock_lst.release()
 
                 #针对1章分多页的情况
                 nextPg = bs.find("a",string="下一页")
@@ -120,17 +130,31 @@ class getInfo:
             for i in range(len_con-3):
                 index=self.bs.find(attrs={'data-id':str(i)}).contents
                 index_container+=index
+        elif self.DOMAIN == "https://www.yinghuatxt.com/":
+            #todo 手动更改目录
+            for i in range(1,178):
+                index=self.bs.find(attrs={'data-rid':str(i)})
+                index_container+=index
         count =0
         for i in range(len(index_container)):
             lst.append(dict())
+        # print(index_container)
+        # print(index_container[0].text)
+        # return
         self.lock = _thread.allocate_lock()
         # for i in range(3):
         #     index = index_container[i]
         for index in tqdm(index_container):
-            chapter = index.find('a')
-            while self.threadwork>=config.THREADNUM:
-                pass
-            id = _thread.start_new_thread(self.getContents,(self.DOMAIN+chapter.get('href'),chapter.text,count,0))
+            if self.DOMAIN == "https://www.yinghuatxt.com/":
+                chapter = index
+                while self.threadwork>=config.THREADNUM:
+                    pass
+                id = _thread.start_new_thread(self.getContents,(self.DOMAIN+chapter.get('href'),chapter.text,count,0))
+            else:
+                chapter = index.find('a')
+                while self.threadwork>=config.THREADNUM:
+                    pass
+                id = _thread.start_new_thread(self.getContents,(self.DOMAIN+chapter.get('href'),chapter.text,count,0))
             count+=1
             threadpool[id]=1
             self.lock.acquire()
@@ -170,8 +194,13 @@ class getInfo:
                     for texts in v:
                         for text in texts:
                             #print(text.text)
-                            fd.write(text.text)
+                            fd.write('\b')
+                            if not text is None:
+                                fd.write(text.text)
+                            else:
+                                fd.write("这章丢失了")
                             fd.write('\n')
+                    fd.write('\n')
                 fd.flush()
             fd.close()
             self.fdIndex.close()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from getInfo import getInfo
 import config
 def main():
-    test = getInfo("https://www.aishuge.la/kan/10/10822/#:~:text=%5B%E7%BB%BC%5D%E5%BF%BD%E6%82%A0%E6%95%91%E4%B8%96%E4%B8%BB%E7%9A%84%E6%AD%A3%E7%A1%AE%E5%A7%BF%E5%8A%BF%E7%AB%A0%E8%8A%82%E5%88%97%E8%A1%A8%20%E7%AC%AC1%E7%AB%A0,%E5%AE%A1%E5%88%A4-%E6%AD%A3%E4%BD%8D%20%E7%AC%AC2%E7%AB%A0%20%E9%AB%98%E5%A1%94-%E9%80%86%E4%BD%8D")
+    test = getInfo("https://www.yinghuatxt.com/ydn296832/")
     test.makeBook(config.TEXT)
     #getContents('https://m.yushubo.com/read_93104_1.html',0)
     pass
